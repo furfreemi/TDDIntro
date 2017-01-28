91859815cd79f7117e6798d6c5c74316cf545341
furfreemi <furfreemi@yahoo.com>
2017-01-28 15:54:53 -0500

    3rd test case passes

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,10 +2,11 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
-        // Test case #1
-        if (i == 1){
+        // Test case #1, 3
+        if (i == 0 || i == 1){
             return 1;
         }
+        // Test case #2
         if (i == 2) {
             return 2;
         }
