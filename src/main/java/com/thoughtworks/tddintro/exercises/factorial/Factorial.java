250778c32316b8002b56a7af9f58f0de2cc8e612
furfreemi <furfreemi@yahoo.com>
2017-01-28 15:56:23 -0500

    5th test case passes

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,6 +2,10 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
+        // Test case #5
+        if (i < 0){
+            throw new IllegalArgumentException();
+        }
         // Test case #1, 3
         if (i == 0 || i == 1){
             return 1;
