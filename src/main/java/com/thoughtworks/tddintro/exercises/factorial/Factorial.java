a350d5192facc6f9fe2aee4fd11c5c70e51ebdbc
furfreemi <furfreemi@yahoo.com>
2017-01-28 15:53:34 -0500

    First test case passes

--- src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
+++ src/main/java/com/thoughtworks/tddintro/exercises/factorial/Factorial.java
@@ -2,6 +2,10 @@ package com.thoughtworks.tddintro.exercises.factorial;
 
 public class Factorial {
     public Integer compute(int i) {
+        // Test case #1
+        if (i == 1){
+            return 1;
+        }
         return -1;
     }
 }
